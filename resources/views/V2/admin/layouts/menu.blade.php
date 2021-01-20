359b778cb5f19dd4816ba3a45b56a2b3d6435fa8
manohisoaa.dev <manohisoa.dev@gmail.com>
2021-01-20 14:49:26 +0300

    imageUrl user dynamic, modification user name et role menu

--- resources/views/V2/admin/layouts/menu.blade.php
+++ resources/views/V2/admin/layouts/menu.blade.php
@@ -1,9 +1,9 @@
 <li class="nav-header">
     <div class="dropdown profile-element">
-        <img alt="image" class="rounded-circle" src="{{ asset('administrator/V2/img/profile_small.jpg') }}"/>
+        <img alt="image" class="rounded-circle" src="{{Auth::user()->imageUrl()}}" width="50"/>
         <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-            <span class="block m-t-xs font-bold">David Williams</span>
-            <span class="text-muted text-xs block">Art Director <b class="caret"></b></span>
+            <span class="block m-t-xs font-bold">{{ucfirst(Auth::user()->name)}}</span>
+            <span class="text-muted text-xs block">{{ucfirst(Auth::user()->role)}}<b class="caret"></b></span>
         </a>
         <ul class="dropdown-menu animated fadeInRight m-t-xs">
             <li><a class="dropdown-item" href="profile.html">Profile</a></li>
