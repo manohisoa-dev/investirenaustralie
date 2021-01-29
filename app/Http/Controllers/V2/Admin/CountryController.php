f3d03658791e8f0168629c5c462a2c493136a9b6
Rado <razafindraiber@gmail.com>
2021-01-29 18:47:23 +0300

    mis à jour views/V2/admin/product/index.blade.php

--- app/Http/Controllers/V2/Admin/CountryController.php
+++ app/Http/Controllers/V2/Admin/CountryController.php
@@ -42,7 +42,7 @@ class CountryController extends Controller
 
         # notification
         Notify::success('Country a été créer avec succès');
-        return redirect(redirect(route('v2.admincountry.index'));
+        return redirect(route('v2.admincountry.index'));
     }
 
     /**
@@ -89,7 +89,7 @@ class CountryController extends Controller
 
         # notification
         Notify::success('Country a été mise à jour avec succès');
-        return redirect(redirect(route('v2.admincountry.index'));
+        return redirect(route('v2.admincountry.index'));
     }
 
     /**
@@ -103,7 +103,7 @@ class CountryController extends Controller
 
         # notification
         Notify::success('Country a été supprimer avec succès');
-        return redirect(redirect(route('v2.admincountry.index'));
+        return redirect(route('v2.admincountry.index'));
     }
 
     protected function view($view, $data = [])
--- app/Http/Controllers/V2/Admin/PostalcodeController.php
+++ app/Http/Controllers/V2/Admin/PostalcodeController.php
@@ -42,7 +42,7 @@ class PostalcodeController extends Controller
 
         # notification
         Notify::success('Postalcode a été créer avec succès');
-        return redirect(redirect(route('v2.adminpostalcode.index'));
+        return redirect(route('v2.adminpostalcode.index'));
     }
 
     /**
@@ -89,7 +89,7 @@ class PostalcodeController extends Controller
 
         # notification
         Notify::success('Postalcode a été mise à jour avec succès');
-        return redirect(redirect(route('v2.adminpostalcode.index'));
+        return redirect(route('v2.adminpostalcode.index'));
     }
 
     /**
@@ -103,7 +103,7 @@ class PostalcodeController extends Controller
 
         # notification
         Notify::success('Postalcode a été supprimer avec succès');
-        return redirect(redirect(route('v2.adminpostalcode.index'));
+        return redirect(route('v2.adminpostalcode.index'));
     }
 
     protected function view($view, $data = [])
