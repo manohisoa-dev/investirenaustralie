c4826a4663e5cba514780eaccb5c67b5ae0f2a04
manohisoaa.dev <manohisoa.dev@gmail.com>
2021-01-29 11:45:45 +0300

    ajout title pour les boutons actions detail, modification et suppression

--- resources/views/vendor/crud/single-page-templates/common/actions.blade.php
+++ resources/views/vendor/crud/single-page-templates/common/actions.blade.php
@@ -4,14 +4,14 @@
 ?>
 <td class="actions-cell text-center" width="7%">
 <form class="form-inline" action="{{$url}}/{{$record->id}}" method="POST">
-    <a href="{{$url}}/{{$record->id}}"><i class="fa fa-eye"></i></a>&nbsp;&nbsp;
+    <a href="{{$url}}/{{$record->id}}" title="Détail"><i class="fa fa-eye"></i></a>&nbsp;&nbsp;
 
-    <a href="{{$url}}/{{$record->id}}/edit"><i class="fa fa-pencil-square-o"></i></a>
+    <a href="{{$url}}/{{$record->id}}/edit" title="Modification"><i class="fa fa-pencil-square-o"></i></a>
 
     {{ csrf_field() }}
     {{ method_field('DELETE') }}
     <button style="outline: none;background: transparent;border: none;"
             onclick="return confirm('Vous êtes sur?')"
-            type="submit" class="fa fa-trash text-danger"></button>
+            type="submit" class="fa fa-trash text-danger" title="Suppression"></button>
 </form>
 </td>
