0ef8a972557e4cdc0693aaf2ee937897ff45de86
manohisoaa.dev <manohisoa.dev@gmail.com>
2021-02-15 11:37:01 +0300

    Correction redirection auto vers page d'accueil, Admin, config

--- app/Http/Controllers/Admin/ConfigController.php
+++ app/Http/Controllers/Admin/ConfigController.php
@@ -24,7 +24,7 @@ class ConfigController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-        $this->middleware('role:admin');
+        $this->middleware('role:1');
     }
 
     /**
