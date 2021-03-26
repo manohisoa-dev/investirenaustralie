e07dadcaa24f132d56cfc18790e99f5d4d1d0baf
dev <landru>
2021-03-26 11:36:48 +0300

    Debug redirection slide pub produit vers null

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -57,8 +57,6 @@ class ProductController extends Controller
                 
                 $page = Page::where('path', '=', '/products*')
                     ->first();
-
-                    dd($page);
                 
                 if($page){$pubs = $page->pubs;}else{$pubs = [];}
                 
