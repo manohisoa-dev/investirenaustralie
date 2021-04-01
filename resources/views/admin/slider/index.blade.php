0947c4d3f328db1478e2a0799b788c854860531b
Rado <razafindraiber@gmail.com>
2021-04-01 11:06:14 +0300

    mis à jour slider admin

--- resources/views/admin/slider/index.blade.php
+++ resources/views/admin/slider/index.blade.php
@@ -36,25 +36,25 @@
                 <table class="table table-striped grid-view-tbl">
                 <thead>
                     <tr class="header-row">
-                                                    {!!\Nvd\Crud\Html::sortableTh('id','admin.slider.index','Id')!!}
-                                                    {!!\Nvd\Crud\Html::sortableTh('content','admin.slider.index','Content')!!}
-                                                    {!!\Nvd\Crud\Html::sortableTh('type','admin.slider.index','Type')!!}
-                                                    {!!\Nvd\Crud\Html::sortableTh('status','admin.slider.index','Status')!!}
-                                                    {!!\Nvd\Crud\Html::sortableTh('image_id','admin.slider.index','Image Id')!!}
-                                                    {!!\Nvd\Crud\Html::sortableTh('created_at','admin.slider.index','Created At')!!}
-                                                    {!!\Nvd\Crud\Html::sortableTh('updated_at','admin.slider.index','Updated At')!!}
-                                                <th><a href="javascript:void(0)">Actions</a></th>
+						{!!\Nvd\Crud\Html::sortableTh('id','admin.slider.index','Id')!!}
+						{!!\Nvd\Crud\Html::sortableTh('image_id','admin.slider.index','Image')!!}
+						{!!\Nvd\Crud\Html::sortableTh('content','admin.slider.index','Content')!!}
+						{!!\Nvd\Crud\Html::sortableTh('type','admin.slider.index','Type')!!}
+						{!!\Nvd\Crud\Html::sortableTh('status','admin.slider.index','Status')!!}						
+						{!!\Nvd\Crud\Html::sortableTh('created_at','admin.slider.index','Created At')!!}
+						{!!\Nvd\Crud\Html::sortableTh('updated_at','admin.slider.index','Updated At')!!}
+						<th><a href="javascript:void(0)">Actions</a></th>
                     </tr>
                     <tr class="search-row">
                         <form class="search-form">
-                                                            <td><input type="text" class="form-control" name="id" value="{{Request::input("id")}}"></td>
-                                                            <td><input type="text" class="form-control" name="content" value="{{Request::input("content")}}"></td>
-                                                            <td><input type="text" class="form-control" name="type" value="{{Request::input("type")}}"></td>
-                                                            <td><input type="text" class="form-control" name="status" value="{{Request::input("status")}}"></td>
-                                                            <td><input type="text" class="form-control" name="image_id" value="{{Request::input("image_id")}}"></td>
-                                                            <td><input type="text" class="form-control" name="created_at" value="{{Request::input("created_at")}}"></td>
-                                                            <td><input type="text" class="form-control" name="updated_at" value="{{Request::input("updated_at")}}"></td>
-                                                        <td style="min-width: 6em;">@include('vendor.crud.single-page-templates.common.search-btn')</td>
+							<td><input type="text" class="form-control" name="id" value="{{Request::input("id")}}"></td>
+							<td><input type="text" class="form-control" name="image_id" value="{{Request::input("image_id")}}"></td>
+							<td><input type="text" class="form-control" name="content" value="{{Request::input("content")}}"></td>
+							<td><input type="text" class="form-control" name="type" value="{{Request::input("type")}}"></td>
+							<td><input type="text" class="form-control" name="status" value="{{Request::input("status")}}"></td>							
+							<td><input type="text" class="form-control" name="created_at" value="{{Request::input("created_at")}}"></td>
+							<td><input type="text" class="form-control" name="updated_at" value="{{Request::input("updated_at")}}"></td>
+							<td style="min-width: 6em;">@include('vendor.crud.single-page-templates.common.search-btn')</td>
                         </form>
                     </tr>
                     </thead>
@@ -62,52 +62,40 @@
                     <tbody>
                         @forelse ( $records as $record )
                             <tr>
-                                                                <td>
-                                                                            {{ $record->id }}
-                                                                    </td>
-                                                                <td>
-                                                                        <span class="editable"
+                                <td>{{ $record->id }}</td>
+								<td>
+                                     <img src="{{ asset($record->images->filepath) }}" class="img-responsive" style="height:80px" />
+                                </td>
+                                <td>
+                                    <span class="editable"
                                           data-type="text"
                                           data-name="content"
                                           data-value="{{ $record->content }}"
                                           data-pk="{{ $record->{$record->getKeyName()} }}"
                                           data-url="{{ route('admin.slider.index')}}/{{ $record->{$record->getKeyName()} }}"
                                           >{{ $record->content }}</span>
-                                                                    </td>
-                                                                <td>
-                                                                        <span class="editable"
+                                </td>
+                                <td>
+                                    <span class="editable"
                                           data-type="text"
                                           data-name="type"
                                           data-value="{{ $record->type }}"
                                           data-pk="{{ $record->{$record->getKeyName()} }}"
                                           data-url="{{ route('admin.slider.index')}}/{{ $record->{$record->getKeyName()} }}"
                                           >{{ $record->type }}</span>
-                                                                    </td>
-                                                                <td>
-                                                                        <span class="editable"
+                                </td>
+                                <td>
+                                     <span class="editable"
                                           data-type="number"
                                           data-name="status"
                                           data-value="{{ $record->status }}"
                                           data-pk="{{ $record->{$record->getKeyName()} }}"
                                           data-url="{{ route('admin.slider.index')}}/{{ $record->{$record->getKeyName()} }}"
                                           >{{ $record->status }}</span>
-                                                                    </td>
-                                                                <td>
-                                                                        <span class="editable"
-                                          data-type="text"
-                                          data-name="image_id"
-                                          data-value="{{ $record->image_id }}"
-                                          data-pk="{{ $record->{$record->getKeyName()} }}"
-                                          data-url="{{ route('admin.slider.index')}}/{{ $record->{$record->getKeyName()} }}"
-                                          >{{ $record->image_id }}</span>
-                                                                    </td>
-                                                                <td>
-                                                                            {{ $record->created_at }}
-                                                                    </td>
-                                                                <td>
-                                                                            {{ $record->updated_at }}
-                                                                    </td>
-                                                                @include( 'vendor.crud.single-page-templates.common.actions', [ 'url' => route('admin.slider.index'), 'record' => $record ] )
+                                 </td>
+                                 <td>{{ $record->created_at }}</td>
+                                 <td>{{ $record->updated_at }}</td>
+                                  @include( 'vendor.crud.single-page-templates.common.actions', [ 'url' => route('admin.slider.index'), 'record' => $record ] )
                             </tr>
                         @empty
                             @include ('vendor.crud.single-page-templates.common.not-found-tr',['colspan' => 8])
