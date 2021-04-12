18654060f53d3decc6aa41631199d851b38b4aef
dev <landru>
2021-04-12 17:31:54 +0300

    LAYOUT DES PAGES D'AFFICHAGE DES PRODUITS INDIVIDUELS - Un slider de photos du produit. Un clic agrandit la photo en cours. Le visiteur peut faire défiler les photos par boutons droite et gauche.

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -60,53 +60,86 @@
                 <div class="col-lg-8">
                   @include('includes.alerts')
                   @if(count($item->images))
-                  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                      <!-- Indicators -->
-                      <ol class="carousel-indicators">
-                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                        <li data-target="#myCarousel" data-slide-to="1"></li>
-                        <li data-target="#myCarousel" data-slide-to="2"></li>
-                      </ol>
-                      <!-- Wrapper for slides -->
-                      <div class="carousel-inner">
-                        <div class="item active">
-                          <img src="{{asset('images/Surfers_Paradise.jpg')}}" alt="..." style="width:100%;">
-                        </div>
-                        <div class="item">
-                          <img src="{{asset('images/caroussel-image-1.jpg')}}" alt="..." style="width:100%;">
-                        </div>
-                        <div class="item">
-                          <img src="{{asset('images/caroussel-image-2.jpg')}}" alt="..." style="width:100%;">
-                        </div>
-                      </div>
-                      <!-- Left and right controls -->
-                      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left"></span>
-                        <span class="sr-only">@lang('app.btn.prev')</span>
-                      </a>
-                      <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right"></span>
-                        <span class="sr-only">@lang('app.btn.next')</span>
-                      </a>
+                  <div id="myCarousel" class="carousel slide w-100" data-ride="carousel">
+                      <div class="carousel-inner w-100" role="listbox">
+                          @foreach ($item->images as $key=>$it)
+                            <div class="carousel-item carousel-item-prod @if($loop->first) active @endif">
+                                <div class="col-md-12 col-sm-12">
+                                    <div class="thumb-wrapper">
+                                        <div class="img-box p-10px-b m-15px-b border-bottom-2 border-color-gray">
+                                          <div class="grid-item product branding">
+                                            <div class="portfolio-box-02">
+                                                <div class="portfolio-img">
+                                                  @php
+                                                      try { 
+                                                        $img_url = App\Models\Image::whereId($it->pivot->image_id)->first()->filepath;
+                                                        if(file_get_contents($img_url))
+                                                        $img_prod=$img_url;
+                                                      } catch (\Throwable $th) {
+                                                          $img_prod=asset('images/iea.png');
+                                                      }   
+                                                  @endphp
+                                                  <a href="javascript:void(0)"><img src="{{asset($img_prod)}}" alt="{{$it->title}}" class="img-fluid imageresource{{ $key }}"></a>
+                                                </div>
+                                                <div class="portfolio-info">
+                                                    <div class="portfolio-desc">
+                                                        <h5><a href="#">{{ $item->title }}</a></h5>
+                                                    </div>
+                                                    <a href="javascript:void(0)" value="{{ $key }}" class="gallery-link pop">
+                                                        <i class="ti-plus"></i>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>    
+                          @endforeach                            
+                          <a class="carousel-control-prev bg-dark w-auto" href="#myCarousel" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                          </a>
+                          <a class="carousel-control-next bg-dark w-auto" href="#myCarousel" role="button" data-slide="next">
+                              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Next</span>
+                          </a>
+                      </div> 
                   </div>
                   @else
-                  <figure class="figure">
-                    @php
-                        try {
-                            if(file_get_contents($item->imageUrl()));
-                            $img=$item->imageUrl();
-                        } catch (\Throwable $th) {
-                            $img=asset('images/iea.png');
-                        }   
-                    @endphp
-                    <img src="{{$img}}" alt="{{$item->title}}" class="img-fluid shadow rounded">
-                    <figcaption class="m-15px-t dark-color"><h4>{{$item->title}}</h4></figcaption>
-                  </figure> 
+                    <div class="col-md-12 col-sm-12">
+                      <div class="thumb-wrapper">
+                          <div class="img-box p-10px-b m-15px-b border-bottom-2 border-color-gray">
+                            <div class="grid-item product branding">
+                              <div class="portfolio-box-02">
+                                  <div class="portfolio-img">
+                                    @php
+                                        try {
+                                            if(file_get_contents($item->imageUrl()));
+                                            $img=$item->imageUrl();
+                                        } catch (\Throwable $th) {
+                                            $img=asset('images/iea.png');
+                                        } 
+                                    @endphp
+                                    <a href="javascript:void(0)"><img src="{{$img}}" alt="{{$item->title}}" class="img-fluid imageresource0"></a>
+                                  </div>
+                                  <div class="portfolio-info">
+                                      <div class="portfolio-desc">
+                                          <h5><a href="#">{{ $item->title }}</a></h5>
+                                      </div>
+                                      <a href="javascript:void(0)" value="0" class="gallery-link pop">
+                                          <i class="ti-plus"></i>
+                                      </a>
+                                  </div>
+                              </div>
+                            </div>
+                          </div>
+                      </div>
+                  </div>
                   @endif
 
                   <section class="property-meta-wrapper common">
-                    <!-- @include('includes.alerts') -->
-                    <div class="row">
+                    <div class="row m-15px-t">
                         <div class="col-sm-6">
                             <form action="{{route('shop.order', ['product'=>$item->slug])}}" method="post">
                                 {{csrf_field()}}
@@ -280,9 +313,27 @@
 </div>
 <!-- End list APL modal -->
 
+<!-- Creates the bootstrap modal where the image will appear -->
+<div class="modal fade" id="imagemodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+      </div>
+      <div class="modal-body">
+        <img src="" id="imagepreview" style="width: auto; height: auto;" >
+      </div>
+    </div>
+  </div>
+</div>
+
 @endsection
     
 @push('script')
+  <link rel="stylesheet" href="{{ asset('carousel/style.css') }}">
+  <link href="{{ asset('plugin/magnific/magnific-popup.css') }}" rel="stylesheet">
+  <script src="{{ asset('carousel/popper.min.js') }}"></script>
+  <script src="{{ asset('carousel/carousel.js') }}"></script>
   <style>
     #map{
       height: 25rem;
@@ -297,7 +348,12 @@
               alert("{{ trans('app.txt.accept_term', ['role'=>'APL']) }}");
           } 
         });
-      
+
+        $(".pop").on("click", function() {
+          var id = $(this).attr('value');
+          $('#imagepreview').attr('src', $('.imageresource'+id).attr('src')); // here asign the image to the modal when the user click the enlarge link
+          $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+        });
   </script>
   <script>
       var _map;
