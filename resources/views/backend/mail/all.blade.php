3156b67afa9a1ae4ab5b790927c4f42cdb3047c4
Rado <razafindraiber@gmail.com>
2021-08-05 18:54:58 +0300

    mis à jour email

--- resources/views/backend/mail/all.blade.php
+++ resources/views/backend/mail/all.blade.php
@@ -21,39 +21,8 @@
                     </div>
                 </div>
             </div>
-        </div>		
-    </div>
-    <div class="col-lg-4 col-xl-4">
-        <div class="profile-content-area m-40px-tb card card-body">
-            <div class="border-bottom-1 border-color-dark-gray m-35px-b p-35px-b">
-                <h5>{{isset($title)?$title:__('app.list.mail')}}</h5>
-                <div class="row">
-                    <div class="col-md-12 m-10px-tb">
-                        <div class="media">
-                            <div class="media-body p-15px-l lh-normal">
-								<p>
-									<a href="{{route(\App\Models\User::find(Auth::id())->roleUser->role_initial.'.mail.list',['filter'=>'inbox'])}}">
-										<i class="fa fa-envelope-open-text m-10px-r" aria-hidden="true"></i> 
-										@lang('app.mail.inbox')
-									</a>
-								</p>
-								<p>
-									<a href="{{route(\App\Models\User::find(Auth::id())->roleUser->role_initial.'.mail.list',['filter'=>'outbox'])}}">
-										<i class="fa fa-paper-plane m-10px-r" aria-hidden="true"></i> @lang('app.mail.outbox')
-									</a>
-								</p>
-								<p>
-									<a href="{{route(\App\Models\User::find(Auth::id())->roleUser->role_initial.'.mail.list',['filter'=>'draft'])}}">
-										<i class="fa fa-edit m-10px-r" aria-hidden="true"></i> @lang('app.mail.draft')
-									</a>
-								</p>
-								<p>
-									<a href="javascript:void(0)" onclick="new_email()">
-										<i class="far fa-envelope m-10px-r"></i> @lang('app.txt.new_mail')
-									</a>
-								</p>
         </div>
-        <div class="col-lg-4 col-xl-4">
+		<div class="col-lg-4 col-xl-4">
             <div class="profile-content-area m-40px-tb card card-body">
                 <div class="border-bottom-1 border-color-dark-gray m-35px-b p-35px-b">
                     <h5>{{isset($title)?$title:__('app.list.mail')}}</h5>
@@ -64,13 +33,18 @@
                                         <p><a href="{{route(\App\Models\User::find(Auth::id())->roleUser->role_initial.'.mail.list',['filter'=>'inbox'])}}"><i class="fa fa-envelope-open-text" aria-hidden="true"></i> @lang('app.mail.inbox')</a></p>
                                         <p><a href="{{route(\App\Models\User::find(Auth::id())->roleUser->role_initial.'.mail.list',['filter'=>'outbox'])}}"><i class="fa fa-paper-plane" aria-hidden="true"></i> @lang('app.mail.outbox')</a></p>
                                         <p><a href="{{route(\App\Models\User::find(Auth::id())->roleUser->role_initial.'.mail.list',['filter'=>'draft'])}}"><i class="fa fa-edit" aria-hidden="true"></i> @lang('app.mail.draft')</a></p>
+										<p>
+											<a href="javascript:void(0)" onclick="new_email()">
+												<i class="far fa-envelope m-10px-r"></i> @lang('app.txt.new_mail')
+											</a>
+										</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div>		
     </div>
 
 @endsection
