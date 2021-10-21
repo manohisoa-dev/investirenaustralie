89cfcc8470a62c6649a92076acfc11ae83721eb4
TianaRazak <deveasy5@gmail.com>
2021-10-21 12:50:53 +0300

    - Resolve merge

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -499,13 +499,9 @@ class ProductController extends Controller {
 
     function save_programme($categorie, $ancienete, $nature, $prix_min, $prix_max, $type_id,
         $display_address, $postalCode, $state_id, $title, $content, $location_id, $fond_dossier,
-<<<<<<< HEAD
-        $status, $type_commission, $commission, $id_afa = 0,$seller_id = 0) {
-=======
         $status, $type_commission, $commission, $id_afa = 0, $id_solicitor, $commencement_dt,
-        $estimated_delvivery_dt, $programme_firb_pre_approved, $programme_pre_approved_sale) {
+        $estimated_delvivery_dt, $programme_firb_pre_approved, $programme_pre_approved_sale,$seller_id) {
 
->>>>>>> 513beb1a6d55eec3bdb599b698457f2d2a4d725b
         $slug = generateSlug($title);
         $programme = new Product();
 
@@ -534,15 +530,12 @@ class ProductController extends Controller {
         $programme->author_id = Auth::user()->id;
         $programme->validated_at = Carbon::now();
         $programme->afaId_possible = $id_afa;
-<<<<<<< HEAD
-        $programme->seller_id = $seller_id;
-=======
         $programme->solicitor_id = $id_solicitor;
         $programme->commencement_dt = $commencement_dt;
         $programme->estimated_delvivery_dt = $estimated_delvivery_dt;
         $programme->programme_firb_pre_approved = $programme_firb_pre_approved;
         $programme->programme_pre_approved_sale = $programme_pre_approved_sale;
->>>>>>> 513beb1a6d55eec3bdb599b698457f2d2a4d725b
+        $programme->seller_id = $seller_id;
         $programme->save();
 
         // // save translation
@@ -672,17 +665,13 @@ class ProductController extends Controller {
         } else {
             $taux_commision = $request->rate_commission;
         }
-
+        
         $id_programme = $this->save_programme($request->cat_programmme_id, $request->ancienneteBien,
             $request->natureBien, $request->prix_min, $request->prix_max, $request->type_id,
-            $request->display_address, $request->postalCode, $request->state_id, $titre_programme,
+            $request->display_address, $request->postalCode, $request->state_id, $request->title_programme,
             $request->description, $id_location, '', 'waiting', $request->commision, $taux_commision,
-<<<<<<< HEAD
-            $id_afa_p,$seller_id);
-=======
             $id_afa_p, $request->solicitor_id, $request->commencement_dt, $request->estimated_delvivery_dt,
-            $request->programme_firb_pre_approved_program,$request->programme_pre_approved_sale);
->>>>>>> 513beb1a6d55eec3bdb599b698457f2d2a4d725b
+            $request->programme_firb_pre_approved_program,$request->programme_pre_approved_sale,$seller_id);
 
         if ($request->dropPhoto) {
             foreach ($request->dropPhoto as $key => $value) {
