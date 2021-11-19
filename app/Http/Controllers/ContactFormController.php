c77f6b641f8265c87ea93e9c904bd0ec30775d7c
TianaRazak <deveasy5@gmail.com>
2021-11-19 17:23:06 +0300

    Update 19-11-21 (2)

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -14,29 +14,28 @@ class ContactFormController extends Controller
     {
         // $this->setCookie($request);
         // \Cookie::forget('name');
-        \Cookie::queue('namexxx', 'value', '');
+        // \Cookie::queue('namexxx', 'value', '');
 
-        \Cookie::queue(\Cookie::forget('namexxx'));
-        $value = \Cookie::get('namexxx');
+        // \Cookie::queue(\Cookie::forget('namexxx'));
+        // $value = \Cookie::get('namexxx');
 
 
-        $wordsToRemove = Badword::pluck('content')->toArray() ;
-        $wordsToRemove = array_flatten($wordsToRemove) ;
-        $filterOptions = array(
-            'strictness' => 'permissive',
-            'also_check' => $wordsToRemove
-        );
+        // $wordsToRemove = Badword::pluck('content')->toArray() ;
+        // $wordsToRemove = array_flatten($wordsToRemove) ;
+        // $filterOptions = array(
+        //     'strictness' => 'permissive',
+        //     'also_check' => $wordsToRemove
+        // );
 
-        $content = 'Lorem ipsum dolor sit amet, point com consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  rakoto@gmail.com 
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt site http://test.fr  mollit anim id est laborum.' ;
+        // $content = 'Lorem ipsum dolor sit amet, point com consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        // Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        // Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  rakoto@gmail.com 
+        // Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt site http://test.fr  mollit anim id est laborum.' ;
 
-        $filter = new BadWordFilter($filterOptions);
-        $cleanString = $filter->clean($content, "#!%^");
+        // $filter = new BadWordFilter($filterOptions);
+        // $cleanString = $filter->clean($content, "#!%^");
 
-        echo $cleanString ;
-        dd("");
+        // echo $cleanString ;
 
         return view('contactform');
     }
