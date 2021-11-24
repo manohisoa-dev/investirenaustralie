43623550d3af9bd9126489b7d10cff16991b2fbd
TianaRazak <deveasy5@gmail.com>
2021-11-24 14:04:26 +0300

    - front > vérifier détail produit il y a des informations qui ne s'affiche pas

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -50,7 +50,7 @@ class ProductController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function index(Request $request, $slug) {
-        $products = Product::where('slug', '=', $slug)->get(['id','area','title','location_id','view_count','author_id']);
+        $products = Product::where('slug', '=', $slug)->get();
         if (sizeof($products) != 0) {
             foreach ($products as $key => $product) {
 
@@ -59,7 +59,7 @@ class ProductController extends Controller {
                     $product->save();
                 }
 
-                $products = Product::orderBy('created_at', 'desc')->ofStatus('published')->isProduct()->take($this->recentSize)->get(['id','reference','slug','title','location_id','min_price','price']);
+                $products = Product::orderBy('created_at', 'desc')->ofStatus('published')->isProduct()->take($this->recentSize)->get();
 
                 $categories = Category::orderBy('created_at', 'desc')->has('products')->withCount('products')->take($this->recentSize)->get(['id','title']);
 
