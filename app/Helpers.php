4fecbb493099fb53f7d127085131e08ebe19908d
TianaRazak <deveasy5@gmail.com>
2021-11-26 14:30:54 +0300

    Update 26-11-2021

--- app/Helpers.php
+++ app/Helpers.php
@@ -738,38 +738,4 @@ if (!function_exists('distance_point')) {
     }
 }
 
-if (!function_exists('nouvelle_afa_disponible_2')) {
-    function nouvelle_afa_disponible_2() {
-        try {
-            $produit_sans_afa = App\Models\Product::wherein('afaId_possible', array(0, ''))->get();
-            foreach ($produit_sans_afa as $produit) {
-                $localisation_product = DB::select("SELECT * FROM `localizations` WHERE `id` = $produit->location_id");
-                $ville_product = $localisation_product[0]->locality;
-                $afa_possible = DB::select("SELECT `users`.id as id_afa FROM `users` LEFT JOIN `localizations` ON `users`.`location_id` = `localizations`.`id` WHERE `users`.`role` = 3 and `localizations`.`locality` = '$ville_product'");
-                if (count($afa_possible) > 0) {
-                    $tab_afa = array();
-                    foreach ($afa_possible as $val) {
-                        $tab_afa[] = $val->id_afa;
-                    }
-                    $id_afa_p = implode(', ', $tab_afa);
-                    App\Models\Product::where('id', $produit->id)->update(['afaId_possible' => $id_afa_p]);
-                } else {
-                    $afa_arround = DB::select("SELECT `users`.id as id_afa FROM `users` LEFT JOIN `localizations` ON `users`.`location_id` = `localizations`.`id` WHERE `users`.`role` = 3");
-                    if (count($afa_arround) > 0) {
-                        $tab_afa = array();
-                        foreach ($afa_arround as $val) {
-                            $tab_afa[] = $val->id_afa;
-                        }
-                        $id_afa_p = implode(', ', $tab_afa);
 
-                        dd($id_afa_p);
-                        App\Models\Product::where('id', $produit->id)->update(['afaId_possible' => $id_afa_p]);
-                    }
-                }
-            }
-        }
-        catch (exception $e) {
-            $e->getMessage();
-        }
-    }
-}
